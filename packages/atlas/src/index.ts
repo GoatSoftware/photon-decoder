a2d33546d0b2c9b5b3879bdf6261465fa280ae59
set.charly@gmail.com <set.charly@gmail.com>
2020-06-22 20:46:13 +0200

    Atlas handle multiple clients

--- packages/atlas/src/index.ts
+++ packages/atlas/src/index.ts
@@ -1,13 +1,11 @@
 import { connect } from 'socket.io-client';
 
-const socket = connect('http://192.168.1.39:6768');
+const socket = connect('http://localhost:6768');
 
 const minX = -450;
 const minY = -450;
 const maxX = 450;
 const maxY = 450;
-
-let point: HTMLElement;
 
 socket.on('handshake', () => {
     socket.emit('handshake', {
@@ -20,19 +18,29 @@ socket.on('handshake', () => {
 function initAtlas() {
     console.log('Atlas initialization');
     socket.on('aoPackage', handlePackage);
-    point = document.getElementsByClassName('point')[0];
 }
 
 function handlePackage(pkg) {
+    
+    let point: HTMLElement = document.getElementsByClassName('point ' + pkg.name)[0];
+
+    if (!point) {
+        point = document.createElement('div');
+        point.classList.add('point');
+        point.classList.add(pkg.name);
+        const map = document.getElementsByClassName('map')[0];
+        map.appendChild(point);
+    }
+
     const coords = {
         x: 0,
         y: 0
     };
     
-    coords.x = ((pkg.coords[0] + maxX) * 100) / (maxX - minX);
-    coords.y = ((pkg.coords[1] + maxY) * 100) / (maxY - minY);
+    coords.x = ((pkg.aoPkg.coords[0] + maxX) * 100) / (maxX - minX);
+    coords.y = ((pkg.aoPkg.coords[1] + maxY) * 100) / (maxY - minY);
     console.log(coords);
     point.style.top = `calc(${coords.y}% - 10px)`;
     point.style.left = `calc(${coords.x}% - 10px)`;
-    point.style.transform = `rotate(${-1 * (pkg.heading + 135)}deg)`;
+    point.style.transform = `rotate(${-1 * (pkg.aoPkg.heading + 135)}deg)`;
 }
--- packages/nexus/src/index.ts
+++ packages/nexus/src/index.ts
@@ -3,13 +3,18 @@ const PORT = 6768;
 const server = listen(PORT);
 console.log(`Starting Nexus on ${PORT}`);
 
-const feeders: Socket[] = [];
-const consumers: Socket[] = [];
+const feeders: user[] = [];
+const consumers: user[] = [];
 
+interface user {
+  name: string;
+  socket: Socket;
+}
 
 interface HandshakePayload {
   token: string;
   type: 'FEEDER' | 'CONSUMER';
+  name: string;
 }
 
 server.on('connection', function(socket) {
@@ -18,9 +23,9 @@ server.on('connection', function(socket) {
     if (validateToken(params.token)) {
       socket.emit('handshakeEnd');
       if (params.type === 'FEEDER') {
-        initFeeder(socket);
+        initFeeder(params.name, socket);
       } else if (params.type === 'CONSUMER') {
-        initConsumer(socket);
+        initConsumer(params.name, socket);
       }
     }
   });
@@ -30,21 +35,32 @@ function validateToken(token: string) {
   return true;
 }
 
-function initFeeder(socket: Socket) {
-  feeders.push(socket);
+function initFeeder(name: string, socket: Socket) {
+  feeders.push({
+    name,
+    socket
+  });
   console.log('Feeder connected');
   
-  socket.on('aoPackage', aoPackageHandler);
+  socket.on('aoPackage', aoPackageHandler(name));
 }
 
-function initConsumer(socket: Socket) {
+function initConsumer(name: string, socket: Socket) {
   console.log('Consumer connected');
-  consumers.push(socket);
+  consumers.push({
+    name,
+    socket
+  });
 }
 
-function aoPackageHandler(aoPkg) {
-  consumers.forEach(i => {
-    i.emit('aoPackage', aoPkg);
-  });
-  console.log(aoPkg);
+function aoPackageHandler(name: string) {
+  return (aoPkg) => {
+    consumers.forEach(i => {
+      i.socket.emit('aoPackage', {
+        name,
+        aoPkg
+      });
+    });
+    console.log(aoPkg);
+  };
 }
