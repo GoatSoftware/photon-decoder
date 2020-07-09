f08e9d99a029540693680977ddab8685c74bc048
set.charly@gmail.com <set.charly@gmail.com>
2020-07-09 23:55:05 +0200

    Allow cors on nexus

--- packages/nexus/src/index.ts
+++ packages/nexus/src/index.ts
@@ -11,6 +11,12 @@ const server = http.createServer(app);
 const options = {};
 const io = socket(server, options);
 
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
 initSocket(io);
 
 initAtlas(app);
