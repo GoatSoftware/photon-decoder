9ecbc05cc33b4f6b8997e403985ba5798d2c6294
Charly <set.charly@gmail.com>
2020-07-10 10:44:29 +0200

    Nexus cors enable

--- packages/nexus/src/index.ts
+++ packages/nexus/src/index.ts
@@ -3,6 +3,7 @@ import express from 'express';
 import socket from 'socket.io';
 import { initSocket } from './scavenger';
 import { initAtlas } from './atlas';
+import cors from 'cors';
 
 const PORT = 6768;
 
@@ -11,11 +12,7 @@ const server = http.createServer(app);
 const options = {};
 const io = socket(server, options);
 
-app.use(function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  next();
-});
+app.use(cors());
 
 initSocket(io);
 
